589b0cc39ae8545534eec2421b8dd7cda76fb9e6
Alaa _est_la <hamid6919196919@gmail.com>
2026-01-03 21:37:42 +0000

    J’ai 2 menu bar ici

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,3 +1,28 @@
-import RootLayout from "../layout";
+import { ReactNode } from "react";
+import {NextIntlClientProvider, useMessages} from 'next-intl';
+import Header from "@/components/layout/header";
+import Footer from "@/components/layout/footer";
+import { Toaster } from "@/components/ui/toaster";
+import { CartProvider } from "@/context/cart-context";
 
-export default RootLayout;
+type Props = {
+    children: ReactNode;
+    params: {locale: string};
+};
+
+export default function LocaleLayout({children, params: {locale}}: Props) {
+    const messages = useMessages();
+
+    return (
+        <NextIntlClientProvider locale={locale} messages={messages}>
+          <CartProvider>
+              <div className="relative flex min-h-dvh flex-col bg-background">
+                <Header />
+                <main className="flex-1">{children}</main>
+                <Footer />
+              </div>
+              <Toaster />
+          </CartProvider>
+        </NextIntlClientProvider>
+    );
+}
